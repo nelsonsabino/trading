48c3b2e97b1b148eefe83ec417cbe23a7c8c67b7
nelsonsabino <sabino.nelson@gmail.com>
2025-06-30 13:42:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAoKtcIsVOcvI5O6gH_14AXL3bF2I6X8Qc",
+  authDomain: "trading-89c13.firebaseapp.com",
+  projectId: "trading-89c13",
+  storageBucket: "trading-89c13.firebasestorage.app",
+  messagingSenderId: "782074719077",
+  appId: "1:782074719077:web:05c07a2b81b0047ef5cf8c"
+};
+
 export const GESTAO_PADRAO = {
     title: "Plano de Gestão", 
     inputs: [
