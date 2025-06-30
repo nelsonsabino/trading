e390bbe0cc8ae0eaa3940d9674e7675a12c4a54e
nelsonsabino <sabino.nelson@gmail.com>
2025-06-30 16:47:40 +0100

    Create teste-firebase.js

---
+++ teste-firebase.js
@@ -0,0 +1,18 @@
+// Versão 1.0
+// Importa as funções do Firebase que precisamos
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+// A sua configuração real do Firebase (pode copiar do seu config.js)
+const firebaseConfig = {
+  apiKey: "AIzaSyAoKtcIsVOcvI5O6gH_14AXL3bF2I6X8Qc",
+  authDomain: "trading-89c13.firebaseapp.com",
+  projectId: "trading-89c13",
+  storageBucket: "trading-89c13.firebasestorage.app",
+  messagingSenderId: "782074719077",
+  appId: "1:782074719077:web:05c07a2b81b0047ef5cf8c"
+};
+
+// Inicializa e exporta a instância da base de dados
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
