8a2acafac4263f07c1b5a185b0783e23e16c9782
nelsonsabino <sabino.nelson@gmail.com>
2025-07-08 11:22:54 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -79,10 +79,5 @@ if (assetSymbolFromUrl) {
         loadAndOpenForEditing(tradeIdToEdit);
     }
 
-    // --- LÓGICA DA VERSÃO ---
-    const APP_VERSION = '6.2.2';
-    const versionDisplay = document.getElementById('app-version-display');
-    if (versionDisplay) {
-        versionDisplay.textContent = `Versão: ${APP_VERSION}`;
-    }
+
 });
